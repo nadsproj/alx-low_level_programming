661280c232c98999ddab999fe60a0299f72d704f
nadsproj <fullimeprojwork@gmail.com>
2023-03-22 10:54:54 -0700

    task1

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,3 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: n is an integer
+ * Description: prints the absolute value of an integer
+ * Return: int
+ */
+
 int _abs(int n)
 {
 	if (n > 0)
@@ -13,3 +22,4 @@ int _abs(int n)
 		return (n);
 	}
 }
+
