6d722e35478734313c57dc2bcd8eeb1340ba47af
nadsproj <fullimeprojwork@gmail.com>
2023-03-27 19:10:10 -0700

    l

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 /**
  * _puts - prints a string and a new line
  * @str: pointer to a string to print
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 /**
  * rev_string - prints a string in reverse
  * @s: the used string to beb reversed
